3113d9190f681a5c016a057e27602311bed4ba7d
github username <github email address>
2018-11-19 13:11:43 -0500

    Cell component WIP

---
+++ src/Cell.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Cell extends Component {
+  constructor(){
+    supper(
+      this.state = {
+
+      }
+    )
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
