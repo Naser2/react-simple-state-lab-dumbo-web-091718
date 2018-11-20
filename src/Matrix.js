104db7e3060a8621c9a5bce36b0395ea50fc37ab
github username <github email address>
2018-11-20 13:57:15 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import Cell from './Cell'
 
 
-class Matrix extends Component {
+class Matrix extends Component { 
   constructor(props) {
     super(props)
     this.state ={
       values: props.values.length ? props.values : [...Array(10).fill(Array(10).fill('#F00'))]
     }
   }
- genRow = (vals) => (
-    vals.map(val => <Cell value={val} />) // replace me and render a cell component instead!
+ genRow = (rowValues) => (
+    rowValues.map(val => <Cell value={val} />) // replace me and render a cell component instead!
   )
   
  genMatrix = () => (
-    this.state.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    this.state.values.map(rowValues => <div className="row">{this.genRow(rowValues)}</div>)
   )
   
   render() {
